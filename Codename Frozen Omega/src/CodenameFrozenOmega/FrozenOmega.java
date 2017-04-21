c1f569a21c01ff7c7869b2d1fc2390c89dcae267
Kaikuh <leevi.ni3mi@gmail.com>
2017-04-21 13:43:48 +0300

    Deleted the test files and created the actual package for the project.

---
+++ Codename Frozen Omega/src/CodenameFrozenOmega/FrozenOmega.java
@@ -0,0 +1,10 @@
+package CodenameFrozenOmega;
+
+public class FrozenOmega {
+
+	public static void main(String[] args) {
+		// This is a useless line of text.
+
+	}
+
+}
--- Codename Frozen Omega/src/Test2.java
+++
@@ -1,4 +0,0 @@
-
-public class Test2 {
-	public String test = "This is uploaded from eclipse";
-}

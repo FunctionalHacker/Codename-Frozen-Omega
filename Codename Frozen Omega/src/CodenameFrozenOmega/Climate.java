0861c924aff6ac7ce69a30d4a626064830c4b618
ReekyMarko <reekymarko@reekynet.com>
2017-04-25 20:17:53 +0300

    Made constructor, setters and getters

--- Codename Frozen Omega/src/CodenameFrozenOmega/Climate.java
+++ Codename Frozen Omega/src/CodenameFrozenOmega/Climate.java
@@ -1,5 +1,29 @@
 package CodenameFrozenOmega;
 
 public class Climate {
+	
+	int temperature,humidity;
+
+	public Climate(int temperature, int humidity) {
+		super();
+		this.temperature = temperature;
+		this.humidity = humidity;
+	}
+
+	public int getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(int temperature) {
+		this.temperature = temperature;
+	}
+
+	public int getHumidity() {
+		return humidity;
+	}
+
+	public void setHumidity(int humidity) {
+		this.humidity = humidity;
+	}
 
 }

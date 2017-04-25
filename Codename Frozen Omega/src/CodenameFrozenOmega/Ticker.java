1d94612309333f5d76952549c37cff873127e28a
ReekyMarko <reekymarko@reekynet.com>
2017-04-25 20:08:40 +0300

    Set up the basics of the ticker class

--- Codename Frozen Omega/src/CodenameFrozenOmega/Ticker.java
+++ Codename Frozen Omega/src/CodenameFrozenOmega/Ticker.java
@@ -1,5 +1,18 @@
 package CodenameFrozenOmega;
 
 public class Ticker {
-
+	
+	int speed;
+	
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	
+	public void start() {
+		
+	}
 }

74104005a73dbd60648e15f53e0010bdc06a0923
ReekyMarko <reekymarko@reekynet.com>
2017-04-25 20:14:28 +0300

    Added getters and setters

--- Codename Frozen Omega/src/CodenameFrozenOmega/Ticker.java
+++ Codename Frozen Omega/src/CodenameFrozenOmega/Ticker.java
@@ -3,11 +3,20 @@ package CodenameFrozenOmega;
 public class Ticker {
 	
 	int speed;
+	
 	public Ticker(int speed) {
 		super();
 		this.speed = speed;
 	}
+	
+	public int getSpeed() {
+		return speed;
+	}
 
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
 
 	public void start() {
 		

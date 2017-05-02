d9fa4a827ec234fbd9dc351f0e676e5010a45c67
ReekyMarko <reekymarko@reekynet.com>
2017-05-02 12:45:53 +0300

    Fixed the mess

--- Codename Frozen Omega/src/CodenameFrozenOmega/Organism.java
+++ Codename Frozen Omega/src/CodenameFrozenOmega/Organism.java
@@ -1,16 +1,17 @@
 package CodenameFrozenOmega;
 
 public class Organism {
-	int preferredTemperature, id, location, lifespan;
-	boolean healthy = true;
+	
+
+	
+	int preferredTemperature, id, location;
 	String species;
 	
-	public Organism(int preferredTemperature, int id, int location, int lifespan, String species) {
+	public Organism(int preferredTemperature, int id, int location, String species) {
 		super();
 		this.preferredTemperature = preferredTemperature;
 		this.id = id;
 		this.location = location;
-		this.lifespan = lifespan;
 		this.species = species;
 	}
 }

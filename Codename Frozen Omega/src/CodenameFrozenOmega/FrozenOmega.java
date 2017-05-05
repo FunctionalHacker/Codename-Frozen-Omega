aba33f68d75cee47dade135a30973a0dd6ed0cc6
Kaikuh <leevi.ni3mi@gmail.com>
2017-05-05 09:20:57 +0300

    Fixing conflicts and stupid mistakes

--- Codename Frozen Omega/src/CodenameFrozenOmega/FrozenOmega.java
+++ Codename Frozen Omega/src/CodenameFrozenOmega/FrozenOmega.java
@@ -1,21 +1,9 @@
 package CodenameFrozenOmega;
 
 import java.util.ArrayList;
-<<<<<<< HEAD
-import java.util.jar.Attributes.Name;
-
-import javax.swing.SwingUtilities;
-=======
->>>>>>> refs/remotes/origin/master
 
 public class FrozenOmega {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-	//Screenmain screen = new Screenmain();
-	jellona = new Carnivore(0, 0, 0, 0, null, 0);
-    ButtonGrid.showGui();
-   
-=======
 	Calc calc = new Calc();
 	//Screenmain screen = new Screenmain();
 	ObjectsControl objectscontrol = new ObjectsControl();
@@ -24,7 +12,6 @@ public class FrozenOmega {
 	ArrayList<Tile> tiles = new ArrayList<>();
 	for(int i=1; i<=4; i++){
 		tiles.add(new Tile(i, 21));
->>>>>>> refs/remotes/origin/master
 	}
   }
 }

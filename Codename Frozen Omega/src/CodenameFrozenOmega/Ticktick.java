ec698197f8e7fbee01f5ff2c57136ee594d7505f
ReekyMarko <reekymarko@reekynet.com>
2017-05-08 14:06:30 +0300

    Testing tickerz

--- Codename Frozen Omega/src/CodenameFrozenOmega/Ticktick.java
+++ Codename Frozen Omega/src/CodenameFrozenOmega/Ticktick.java
@@ -6,6 +6,7 @@ public class Ticktick {
 	Calc calc = new Calc();
 	ObjectsControl control = new ObjectsControl();
 	static int tickcount=0;
+	int count50=0;
 	public void Start(int rate){
 		TickerEng ticker = new TickerEng(rate); //ticks per second
 
@@ -25,20 +26,34 @@ public class Ticktick {
 		    public void onTick(float deltaTime) {
 		    	
 		    	if(tickcount == 0){
-		    		
-		    		control.createObjects("Herbivore", "Koala", 50, 2, 21, 1, 20, 0, 0, 0,true);
+		    		//control.createObjects(className, species, amount, id, preferredTemperature,
+		    		//						location, lifespan, age, foodChainPosition, habitability, healthy);
+		    		control.createObjects("Herbivore", "Koala", 50, 2, 21, 1, 20, 20, 0, 0,true);
 		    		control.createObjects("Carnivore", "Joku", 5, 1, 20, 0, 7300, 0, 0, 0, true);
 		    		for(int i=0; i<=4; i++){
 		    			tiles.add(new Tile(i, 21));
 		    		}
 		    	}
-		    
-		    	tiles.get(0).setTemperature(10);
+		    	//MANDATORY FOR EACH RUN
 		    	control.setHabitabilities(calc, control, tiles);
 		    	control.move(control, calc, tiles);
 		    	control.age(control, calc);
-		    	tickcount++;
+		    	control.removeDeadObjects(control);
+		    	///////////////////
+		    	
+		    	tiles.get(0).setTemperature(10);
 
+		    	System.out.println("Herbivores:"+control.herbivore.size());
+		    	
+		    	//COUNTERS
+		    	count50 ++;
+		    	if(count50 >= 50){
+		    		System.out.println("50 TICKS!!!!");
+		    		count50 = 0;
+		    	}
+		    	System.out.println("Tick: "+tickcount);
+		    	tickcount++;
+		    	/////////////
 		    	
 		    }
 		});
